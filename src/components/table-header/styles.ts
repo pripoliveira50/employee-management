6d92e93531ca2afabf81bc447cb1071287d9e837
Priscila Oliveira <priscilapoliveira50@gmail.com>
2025-02-25 15:22:12 -0300

    styles: refac styles table header

--- src/components/table-header/styles.ts
+++ src/components/table-header/styles.ts
@@ -1,24 +1,56 @@
 import { StyleSheet } from "react-native";
+import { tokens } from "../../global/tokens";
 
 export const styles = StyleSheet.create({
-  headerContainer: {
+  container: {
+    borderWidth: 1,
+    borderRadius: tokens.spacing.Md,
+    borderColor: tokens.colors.gray10,
+    marginVertical: tokens.spacing.Sml,
+    backgroundColor: tokens.colors.white,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: tokens.spacing.Md,
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: tokens.spacing.Sml,
+  },
+  name: {
+    flex: 1,
+    fontSize: tokens.typography.h3.fontSize,
+    fontWeight: tokens.typography.h3.fontWeight,
+  },
+  details: {
+    padding: tokens.spacing.Md,
+    borderTopWidth: 1,
+    borderTopColor: tokens.colors.gray05,
+  },
+  infoContainer: {
+    flexDirection: "column",
+    gap: tokens.spacing.Sml,
+  },
+  infoRow: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    padding: 16,
-    backgroundColor: "#E6E6FA",
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8,
+    paddingVertical: tokens.spacing.Xxs,
+    borderBottomWidth: 1,
+    borderBottomColor: tokens.colors.gray05,
   },
-  headerContent: {
-    flexDirection: "row",
-    alignItems: "center",
+  label: {
+    fontWeight: tokens.typography.h2.fontWeight,
+    fontSize: tokens.typography.h2.fontSize,
   },
-  spacer: {
-    width: 50,
+  value: {
+    fontWeight: tokens.typography.h3.fontWeight,
+    textAlign: "right",
   },
-  headerText: {
-    fontWeight: "bold",
-    fontSize: 16,
+  space: {
+    marginLeft: tokens.spacing.Xxl,
   },
 });
