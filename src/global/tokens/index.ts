f60d06eb0443ff5b26f725ecb92ab0d2ab15441f
Priscila Oliveira <priscilapoliveira50@gmail.com>
2025-02-25 15:15:14 -0300

    feat: add tokens

---
+++ src/global/tokens/index.ts
@@ -0,0 +1,37 @@
+export const tokens = {
+  colors: {
+    primary: "#003CFF",
+    blue10: "#E6E8FF",
+    black: "#161616",
+    gray20: "#4F4F4F",
+    gray10: "#808080",
+    gray05: "#E0E0E0",
+    gray00: "#F5F5F5",
+    white: "#FFFFFF",
+  },
+  typography: {
+    fontFamily: "Helvetica",
+    h1: {
+      fontSize: 20,
+      fontWeight: "600",
+    },
+    h2: {
+      fontSize: 18,
+      fontWeight: "600",
+    },
+    h3: {
+      fontSize: 16,
+      fontWeight: "400",
+    },
+  },
+  spacing: {
+    Xxs: 2,
+    Xs: 4,
+    Sml: 8,
+    Md: 12,
+    Lg: 16,
+    Xl: 24,
+    Xxl: 32,
+    Xxxl: 40,
+  },
+};
